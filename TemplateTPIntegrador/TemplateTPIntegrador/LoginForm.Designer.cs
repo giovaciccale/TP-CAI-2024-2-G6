e369003f3a405567a38827414f70d34472ae5746
giovaciccale <giovaciccale@hotmail.com>
2024-09-06 21:02:15 -0300

    Validaciones
    Se creo el utils del Template y la Capa Negocio. Se crearon métodos para aplicar al Login.

---
+++ TemplateTPIntegrador/TemplateTPIntegrador/LoginForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TemplateTPIntegrador
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        // Declaración de txt_contraseña como un campo de clase
+        private System.Windows.Forms.TextBox txt_contraseña;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_contraseña = new System.Windows.Forms.TextBox();  // Ahora está fuera del método y accesible para toda la clase
+            this.btn_iniciarSesion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USUARIO";
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(342, 139);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(166, 20);
+            this.txt_usuario.TabIndex = 1;
+            this.txt_usuario.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CONTRASEÑA";
+            // 
+            // txt_contraseña
+            // 
+            this.txt_contraseña.Location = new System.Drawing.Point(342, 173);
+            this.txt_contraseña.Name = "txt_contraseña";
+            this.txt_contraseña.Size = new System.Drawing.Size(166, 20);
+            this.txt_contraseña.TabIndex = 3;
+            // 
+            // btn_iniciarSesion
+            // 
+            this.btn_iniciarSesion.Location = new System.Drawing.Point(243, 223);
+            this.btn_iniciarSesion.Name = "btn_iniciarSesion";
+            this.btn_iniciarSesion.Size = new System.Drawing.Size(265, 57);
+            this.btn_iniciarSesion.TabIndex = 4;
+            this.btn_iniciarSesion.Text = "INICIAR SESION";
+            this.btn_iniciarSesion.UseVisualStyleBackColor = true;
+            this.btn_iniciarSesion.Click += new System.EventHandler(this.btn_iniciarSesion_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_iniciarSesion);
+            this.Controls.Add(this.txt_contraseña);  // Agregado a los controles
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_iniciarSesion;
+    }
+}
---
+++ TemplateTPIntegrador/TemplateTPIntegrador/utils/validacionesUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemplateTPIntegrador.utils
+{
+    public class ValidacionesTemplateUtils
+    {
+        public bool ValidarStringVacio(string dato)
+        {
+            if (dato == "")
+            {
+                MessageBox.Show("El campo no puede estar vacío.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
