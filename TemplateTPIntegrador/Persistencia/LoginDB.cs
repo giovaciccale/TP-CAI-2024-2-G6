134976319da7b7ddead74d971faf1a8507ac54e1
giovaciccale <giovaciccale@hotmail.com>
2024-10-17 00:00:23 -0300

    Intentos Login
    Con este cambio el usuario solo puede ingresar de forma incorrecta tres veces la contraseña de lo contrario arroja un error.

---
+++ TemplateTPIntegrador/Persistencia/LoginDB.cs
@@ -0,0 +1,33 @@
+﻿using Persistencia.Utils;
+using System;
+
+namespace Persistencia
+{
+    public class LoginDB
+    {
+        public void guardarIntento(String username)
+        {
+            DBHelper dbHelper = new DBHelper("intentos_login");
+            dbHelper.Insertar(username, "1"); // Guarda el primer intento
+        }
+
+        public void actualizarIntento(String key, String newValue)
+        {
+            DBHelper dbHelper = new DBHelper("intentos_login");
+            dbHelper.Modificar(key, newValue); // Actualiza el valor del intento existente
+        }
+
+        public int obtenerIntentos(String username)
+        {
+            DBHelper dbHelper = new DBHelper("intentos_login");
+            String valor = dbHelper.Buscar(username);
+
+            if (valor == null)
+            {
+                valor = "0";
+            }
+
+            return int.Parse(valor);
+        }
+    }
+}
--- TemplateTPIntegrador/TemplateTPIntegrador/Usuarios/LoginForm.Designer.cs
+++ TemplateTPIntegrador/TemplateTPIntegrador/Usuarios/LoginForm.Designer.cs
@@ -154,7 +154,7 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(500, 600);
             this.panel2.TabIndex = 0;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+  
             // 
             // panel4
             // 
