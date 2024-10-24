e6c46f4a6e0618232cd380da715b729321b567de
Roberto Molteni <72MO43598972@campus.economicas.uba.ar>
2024-10-24 18:03:24 -0300

    Mejoras de UI y UX
    Mejoras en el menu del administrador, se agregaron las secciones de las funcionalidades correspondientes yla logica para navegar la seccion de Usuarios

---
+++ TemplateTPIntegrador/TemplateTPIntegrador/Usuarios/Aministrador/ModificarUsuariosForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace TemplateTPIntegrador.Usuarios.Aministrador
+{
+    partial class ModificarUsuariosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ModificarUsuariosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 600);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ModificarUsuariosForm";
+            this.Text = "Modificar Usuarios";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
